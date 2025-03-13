e93d850134c36f81193b14d52233b868c2defc8e
moksh555 <57554299+moksh555@users.noreply.github.com>
2025-03-13 12:48:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const port = 3000;
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "world",
-  password: "Tarupost@200129",
+  database: {your databse},
+  password: {your password},
   port: 5432,
 });
 db.connect();
